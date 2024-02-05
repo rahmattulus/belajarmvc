f4dc61f14e8ed9c962332e434146269173b9d080
rahmattulus <rahmattuluz01@gmail.com>
2024-02-05 20:05:31 +0700

    tambah menu

---
+++ app/view/home/pemasukan.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KasWoy</title>
+    <link rel="shortcut icon" href="../img/logo kas woy 1;1-01.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/4f10ca1bd3.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Doppio+One&family=Inter&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<style>
+    body {
+        background-color: #D1F6F6;
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=Doppio+One&family=Inter&display=swap');
+
+    /* .atas{
+      
+    } */
+
+    .table{
+        border-radius: 10px;
+    }
+
+    h1 {
+        font-family: 'Doppio One';
+    }
+
+    .profil {
+        font-family: 'Doppio One';
+    }
+
+    th td {
+        font-family: 'Inter';
+        align-items: center;
+
+    
+    }
+</style>
+
+<body>
+
+    <nav class="navbar navbar-lg navbar-light">
+        <div class="container d-flex justify-content-between align-items-center">
+          <a class=" ms-0" href="landingPage.html">
+            <img class="logo" src="../img/KAS WOY 1;2-01.png" alt="" style="width: 150px;">
+            </a>
+            <div class="d-flex align-items-center">
+              <div class="text-end me-2">
+                <p class="m-0">Nama</p>
+                <p class="m-0">XI PPLG 1</p>
+              </div>
+              <i class="fas fa-user fa-2x"></i>
+            </div>
+        </div>
+      </nav>
+      <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h4 class="m-0 font-weight-bold text-primary">Pemasukan</h4>
+
+                                    </form>
+                                </div>
+      
+
+    <div class="table p-3" style="background-color: #fff;">
+        <table class="main table text-center">
+            <div class="form-floating mb-3" style="max-width: 35%;">
+                <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                  <option selected>Open this select Month</option>
+                  <option value="01">Januari</option>
+                    <option value="02">Februari</option>
+                    <option value="03">Maret</option>
+                    <option value="04">April</option>
+                    <option value="05">Mei</option>
+                    <option value="06">Juni</option>
+                    <option value="07">Juli</option>
+                    <option value="08">Agustus</option>
+                    <option value="09">September</option>
+                    <option value="10">Oktober</option>
+                    <option value="11">November</option>
+                    <option value="12">Desember</option>
+                </select>
+                <label for="floatingSelect">Pilih Bulan</label>
+              </div>
+
+              <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>Nama</th>
+                                            <th>Minggu ke-1</th>
+                                            <th>Minggu ke-2</th>
+                                            <th>Minggu ke-3</th>
+                                            <th>Minggu ke-4</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Eko</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                
+                                            </div>
+                                        </tr>
+                                        <tr>
+                                            <td>Amanda</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                
+                                            </div>
+                                        </tr>
+                                        <tr>
+                                            <td>Syahrul</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                            </div>
+                                        </tr>
+                                        <tr>
+                                            <td>Serafina</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                
+                                            </div>
+                                        </tr>
+                                         <tr>
+                                            <td>Naufal</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                
+                                            </div>
+                                        </tr>
+                                         <tr>
+                                            <td>Fandy</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                
+                                            </div>
+                                        </tr>
+                                         <tr>
+                                            <td>Rahmat</td>
+                                            <div class="form-check">
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                <td><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></td>
+                                                
+                                            </div>
+</tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+            
+
+</body>
+
+</html>
